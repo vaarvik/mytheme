7352f6b97b56482855a9b6182557776b07cd8665
vaarvik <johannvaarvik@hotmail.com>
2020-10-02 20:10:29 +0200

    Gulp watches all html and php files, not only the one at same directory as gulpfile.js. Style created from SCSS is now enqueued.

--- wp-content/themes/mytheme/inc/enqueue-styles.php
+++ wp-content/themes/mytheme/inc/enqueue-styles.php
@@ -6,7 +6,8 @@ function mytheme_register_styles() {
 
 	$theme_version = wp_get_theme()->get( 'Version' );
 
-	wp_enqueue_style( 'mytheme-style', get_stylesheet_uri(), array(), $theme_version );
+	wp_enqueue_style( 'mytheme-info', get_stylesheet_uri(), array(), $theme_version );
+	wp_enqueue_style( 'mytheme-style', get_stylesheet_directory_uri() . "/assets/styles/style.css", array(), $theme_version );
 
 }
 
